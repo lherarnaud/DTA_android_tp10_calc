09d088a001c4f2fe4d327529453498e91ce4db58
Arnaud L'her <arnaud.lher@laposte.net>
2017-06-14 09:51:15 +0200

    Create layout for RPN Calculator

--- app/src/main/java/com/example/admin/dta_android_tp10_calc/MainActivity.java
+++ app/src/main/java/com/example/admin/dta_android_tp10_calc/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.admin.dta_android_tp10_calc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.GridView;
+import android.widget.ListAdapter;
+import android.widget.TableLayout;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        Button[] buttons = new Button[];
+//
+//        createLayoutButtons();
+
     }
+
+//    private void createLayoutButtons() {
+//        TableLayout layout_buttons = (TableLayout) findViewById(R.id.table_buttons);
+//
+//        layout_buttons.setLayoutParams(new TableLayout.LayoutParams(4, 5));
+//    }
 }
